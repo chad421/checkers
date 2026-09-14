943044183f2d5602e44718be0796fee7d151d7b0
chad421 <jonneh.devine@gmail.com>
2026-09-13 22:12:18 -0400

    increased comment readibility and usefulness

--- src/app.js
+++ src/app.js
@@ -12,24 +12,26 @@ function createBoard() {
 
     // Generate board and id values
     for(let i = 0; i < SLOTS; i++) {
-        // Template variables to populate game container
+        // Template elements to populate game container
         const slot = document.createElement("div")
         const piece = document.createElement("a")
 
+        // Slot ids are 1-65 (64 slots, 8x8)
         slot.id = i + 1
 
-        // Calculates row and column from a 
+        // Calculates row and column from a 1D array
         const numRows = Math.sqrt(SLOTS)
         const row = Math.floor(i/numRows)
         const col = i % numRows
 
-
+        // Checkerboard math pattern generated 
         if((row + col) % 2 == 0) {
             slot.className = "slot slot-light"
         } else {
             slot.className = "slot slot-dark"
         }
 
+        // Sets the top pieces to dark, bottom pieces to light
         if((i <= 24) && (row + col) % 2 == 0) {
             piece.className = "piece piece-dark"
             slot.append(piece)
@@ -46,21 +48,18 @@ function createBoard() {
 
     // Add event listener for board
     gameContainer.addEventListener("click", function (e) {
-        /*
-        e.target returns deepest nested item clicked
-        e.currentTarget returns a copy of the parents element
-        */
-
         const clicked = e.target.closest("div")
-
-        // Checks if a peice is not on the clicked slot
+        
+        // Checks if peice is on the slot clicked
         if(!clicked.querySelector("a")) {
             console.log("invalid click")
-        } else {
-            console.log("valid click!")
         }
 
-        // Checks if slot is on the edge
+        // How many spaces from a peice to move left or right
+        const SPACES_LEFT = Number(clicked.id) + 7
+        const SPACES_RIGHT = Number(clicked.id) + 9
+
+        // Checks if peice is on the edge
         if(Number(clicked.id) % 8 === 0) {
             console.log("You clicked a slot on the rightmost edge!")
         } else if (Number(clicked.id) % 8 === 1) {
